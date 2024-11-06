0e4d7532013d78846c988fccc666dc75b61e2c76
yintong-zhou <zhouyintong96@gmail.com>
2024-11-06 23:00:09 +0100

    settings pokedex structure

--- MyPokedex/Services/PokeApiService.cs
+++ MyPokedex/Services/PokeApiService.cs
@@ -6,46 +6,65 @@ using MyPokedex.Models;
 
 public static class PokeApiService
 {
-    // Instanza statica di HttpClient condivisa tra tutti i metodi della classe
     private static readonly HttpClient HttpClient = new HttpClient
     {
         BaseAddress = new Uri("https://pokeapi.co/api/v2/")
     };
 
+    private static JsonSerializerOptions options = new JsonSerializerOptions
+    {
+        PropertyNamingPolicy = JsonNamingPolicy.CamelCase,
+        PropertyNameCaseInsensitive = true
+    };
+
     // Metodo statico per ottenere i dettagli di un Pokémon
     public static async Task<Pokemon> GetAllPokemonAsync()
     {
-        var response = await HttpClient.GetStringAsync($"pokemon/");
-        return JsonSerializer.Deserialize<Pokemon>(response);
+        try
+        {
+            var response = await HttpClient.GetStringAsync($"pokemon/");
+            return JsonSerializer.Deserialize<Pokemon>(response);
+        }
+        catch (Exception ex)
+        {
+            return null;
+        }
     }
 
     public static async Task<Pokemon> GetPokemonAsync(string nameOrId)
     {
-        var response = await HttpClient.GetStringAsync($"pokemon/{nameOrId}");
-        return JsonSerializer.Deserialize<Pokemon>(response);
+        try
+        {
+            var response = await HttpClient.GetStringAsync($"pokemon/{nameOrId}");
+            return JsonSerializer.Deserialize<Pokemon>(response);
+        }
+        catch (Exception ex)
+        {
+            return null;
+        }
     }
 
     public static async Task<Pokedex> GetAllPokedexAsync()
     {
-        var response = await HttpClient.GetStringAsync($"pokedex");
-        return JsonSerializer.Deserialize<Pokedex>(response);
+        try
+        {
+            var response = await HttpClient.GetStringAsync($"pokedex");
+            return JsonSerializer.Deserialize<Pokedex>(response);
+        }
+        catch (Exception ex) 
+        {
+            return null;
+        }
     }
 
     public static async Task<Pokedex> GetPokedexAsync(string nameOrId)
     {
         try
         {
-            var options = new JsonSerializerOptions
-            {
-                PropertyNamingPolicy = JsonNamingPolicy.CamelCase,
-                PropertyNameCaseInsensitive = true
-            };
-
             var response = await HttpClient.GetStringAsync($"pokedex/{nameOrId}");
-            var result = JsonSerializer.Deserialize<Pokedex>(response, options);
-            return result;
+            return JsonSerializer.Deserialize<Pokedex>(response, options);
         }
-        catch (Exception ex) 
+        catch (Exception ex)
         {
             return null;
         }
